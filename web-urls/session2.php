2547ad17f650b73c65410ac97d0a53a909436d0b
DanishKakar <danishkakar000@gmail.com>
2023-11-11 12:04:01 +0430

    session file added

---
+++ web-urls/session2.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+
+    echo $_SESSION["name"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session 2</title>
+</head>
+<body>
+    
+</body>
+</html>
